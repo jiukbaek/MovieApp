d078b2cdf9be558938157ff84c7c80f8b70147a3
JIUKBAEK <jiuk205@naver.com>
2019-07-23 09:27:01 +0900

    movie app set up

---
+++ src/Movie.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './Movie.css';
+
+class Movie extends Component{
+    render(){
+        return(
+            <div>
+                <MoviePoster />
+                <h1>hello this is a movie</h1>
+            </div>
+        );
+    }
+}
+
+class MoviePoster extends Component{
+    render(){
+        return(
+            <img src="https://www.edupang.com/data_board/2015/06/12/ccb5bed172bc7610fdabd151f33bc313.jpg" />
+        );
+    }
+}
+
+export default Movie;
